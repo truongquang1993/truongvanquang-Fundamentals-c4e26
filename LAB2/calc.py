77ea09c4b4d0179f4f6d7696deed8add9afcf4d3
unknown <truongquanggt@gmail.com>
2019-03-02 01:45:18 +0700

    initial commit

---
+++ LAB2/calc.py
@@ -0,0 +1,17 @@
+def evaluate(x, y, pt):
+    result = 0
+    if op == "+":
+        result_T = x + y
+        # break
+    elif op == "-":
+        result_T = x - y
+        # break
+    elif op == "*":
+        result_T = x * y
+        # break
+    elif op == "/":
+        result_T = x / y
+        # break
+    return result_T
+ 
+        
